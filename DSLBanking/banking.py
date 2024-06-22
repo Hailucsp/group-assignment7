3b8721cb7814ccc7e29bbff8f45fcf08076249db
wahlersj <150095256+wahlersj@users.noreply.github.com>
2024-06-22 17:09:53 -0500

    Update banking.py

--- DSLBanking/banking.py
+++ DSLBanking/banking.py
@@ -1,5 +1,19 @@
-def main():
- print("Hello")
+Create an ini�aliza�on method that creates 5-8 accounts and fills them with money. You can
+read this from a file or hardcode the informa�on.
+• Create a while loop with the following menu op�ons:
+o Ask for the account ID (You will need to output these somewhere so when I test your
+code, I know what to try)
+ Make a deposit.
+ Make a withdrawal.
+ Check the balance.
+ Ask if they would like another op�on or log out of their account.
+o Ask if the user would like to check another account.
+o Ask if the user would like to create a new account.
+o DO NOT create an infinite loop. Use logic and create keywords that will cause your loop
+to exit.
+• Create another while loop (or add it to the above while loop) that runs the specifica�on
+tests.
+• End of program
 
 if __name__ == "__main__":
     print(parse(argv[1]))
