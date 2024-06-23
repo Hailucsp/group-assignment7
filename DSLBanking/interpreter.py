ba075252e4499875b35a6bb0938e0e03370d957c
Hailucsp <hailuc@csp.edu>
2024-06-23 17:10:56 -0500

    Update interpreter.py

--- DSLBanking/interpreter.py
+++ DSLBanking/interpreter.py
@@ -1,49 +1,50 @@
-def lexer(contents):
-    lines = contents.split('\n')
+class Interpreter():
+    def lexer(contents):
+        lines = contents.split('\n')
 
-    nLines = []
-    for line in lines:
-        chars = list(line)
-        tokens = []
-        temp_str = ""
-        quote_count = 0
-        for char in chars:
-            if char == '"' or char == "'":
-                quote_count += 1
-            if quote_count % 2 ==0:
-                in_quotes = False
-            else:
-                in_quotes = True
-
-            if cha4r == " " and in_quotes == False:
-                tokens.append(temp_str)
-                temp_str = ""
-            else:
-                temp_str += char
-        tokens.append(temp_str)
-        items = []
-        for token in tokens:
-            if token[0] == "'" or token[0] == '"':
-                if token[-1] == '"' or toekn[-1] == "'":
-                    itmes.append(("string", token))
+        nLines = []
+        for line in lines:
+            chars = list(line)
+            tokens = []
+            temp_str = ""
+            quote_count = 0
+            for char in chars:
+                if char == '"' or char == "'":
+                    quote_count += 1
+                if quote_count % 2 ==0:
+                    in_quotes = False
                 else:
-                    break
-            elif re.match(r"[.a-zA-Z]+", token):
-                items.append(("symbol", token))
-            elif token in "+-*/=":
-                items.append(("expression", token))
-            elif re.match(r"[.0-9]+", token):
-                items.append(("number", token))
-        nLines.append(items)
-    return nLine
+                    in_quotes = True
+
+                if char == " " and in_quotes == False:
+                    tokens.append(temp_str)
+                    temp_str = ""
+                else:
+                    temp_str += char
+            tokens.append(temp_str)
+            items = []
+            for token in tokens:
+                if token[0] == "'" or token[0] == '"':
+                    if token[-1] == '"' or token[-1] == "'":
+                        items.append(("string", token))
+                    else:
+                        break
+                elif re.match(r"[.a-zA-Z]+", token):
+                    items.append(("symbol", token))
+                elif token in "+-*/=":
+                    items.append(("expression", token))
+                elif re.match(r"[.0-9]+", token):
+                    items.append(("number", token))
+            nLines.append(items)
+        return nLines
 
 Symbols = [
-    "Account Number",
+    "var",
     "function",
     "print"
 ]
 
-Account Number = {
+Vars = {
     
 }
 
@@ -59,27 +60,30 @@ def parse(file):
             if token[0] == 'symbol':
                 if token[1] in Symbols:
                     if token[1] == 'var':
-                        inst_line =+ 'Account Number'
+                        inst_line =+ 'Vars["$v"]'
                     elif token[1] == 'print':
-                        inst_line == 'print'
+                        inst_line == 'print($v)'
                 else:
                     if arrVal(line, y+1)[1] == '=':
-                        if line[y-1][1] == 'Account Number':
+                        if line[y-1][1] == 'var':
                             if token[1] in Vars:
+                                # throw error
                                 break
                             else:
                                 if re.match(r'[.a-zA-Z0-9_]+', token[1]):
-                                    inst_line = inst_line.replace(token[1])
+                                    inst_line = inst_line.replace('$v', token[1])
                                 else:
+                                    # throw error
                                     break
                         else:
-                            if token[1] in Account Number:
-                                inst_line = 'Account Number["'+ token[1] + '"]'
+                            if token[1] in Vars:
+                                inst_line = 'Vars["'+ token[1] + '"]'
                             else:
+                                # throw error
                                 break
                     else:
                         if token[1] in Vars:
-                            inst_line = inst_line.replace('$v', str(Account Number[token[1]]))
+                            inst_line = inst_line.replace('$v', str(Vars[token[1]]))
             elif token[0] == 'expression':
                 inst_line += token[1]
             elif token[0] == 'number':
